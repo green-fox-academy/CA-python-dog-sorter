bef48d74a1ff0427cfd5e1dc0dfc13d2c4de319c
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-14 13:41:03 +0200

    feat(sorter): add dog list

---
+++ sorter.py
@@ -0,0 +1,12 @@
+dogs = [
+{"name": "Bodri", "age": 9},
+{"name": "Buksi", "age": 3},
+{"name": "Burkus", "age": 11},
+{"name": "Lolka", "age": 2},
+{"name": "Killer", "age": 7},
+{"name": "Alma", "age": 5},
+{"name": "Korte", "age": 2},
+{"name": "Morzsa", "age": 1},
+]
+
+print(dogs)
