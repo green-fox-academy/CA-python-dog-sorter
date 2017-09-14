09d410e8371aa219b67139128436486cd1b47b20
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-14 15:01:48 +0200

    feat(sorter): add different comparator

--- sorter.py
+++ sorter.py
@@ -1,18 +1,19 @@
 dogs = [
-{"name": "Bodri", "age": 9},
-{"name": "Lolka", "age": 7},
-{"name": "Killer", "age": 2},
-{"name": "Hektor", "age": 1}
+{"name": "Bodri", "age": 9, "gender": "female"},
+{"name": "Lolka", "age": 7, "gender": "male"},
+{"name": "Killer", "age": 2, "gender": "female"},
+{"name": "Hektor", "age": 1, "gender": "male"},
+{"name": "Korte", "age": 1, "gender": "female"}
 ]
 
 def printer(dogs):
 	for dog in dogs:
-		print(dog["name"] + ", " + str(dog["age"]))
-	print("")	
+		print(dog["name"] + ", " + str(dog["gender"]))	
 
 
 def is_in_order(this, that):
-	return this["age"] <= that["age"]	
+	return this["gender"] == "female"
+	# return this["age"] <= that["age"]	
 
 
 def swapper(this, that):
@@ -22,12 +23,19 @@ def swapper(this, that):
 
 def bubble_sort(dogs):
 	for i in range(len(dogs)):
-		printer(dogs)
 		for j in range(len(dogs)-1-i):
 			if not is_in_order(dogs[j], dogs[j+1]):
 				swapper(dogs[j], dogs[j+1])
 
+def insertion_sort(dogs):
+    for i in range(1,len(dogs)):    
+        j = i                   
+        while j > 0 and is_in_order(dogs[j], dogs[j-1]) : 
+            swapper(dogs[j], dogs[j-1]) 
+            j -= 1 
+
+
 
 
 bubble_sort(dogs)
-		
+printer(dogs)		
