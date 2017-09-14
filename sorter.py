8416ea8c47b42b20441654470e67bb4520e77544
gy0p4k <gyorgypakozdi@gmail.com>
2017-09-14 14:35:27 +0200

    feat(sorter): remove some dogs, make it cool

--- sorter.py
+++ sorter.py
@@ -1,21 +1,18 @@
 dogs = [
 {"name": "Bodri", "age": 9},
-{"name": "Buksi", "age": 3},
-{"name": "Burkus", "age": 11},
-{"name": "Lolka", "age": 2},
-{"name": "Killer", "age": 7},
-{"name": "Alma", "age": 5},
-{"name": "Korte", "age": 2},
-{"name": "Morzsa", "age": 1}
+{"name": "Lolka", "age": 7},
+{"name": "Killer", "age": 2},
+{"name": "Hektor", "age": 1}
 ]
 
 def printer(dogs):
 	for dog in dogs:
 		print(dog["name"] + ", " + str(dog["age"]))
+	print("")	
 
 
 def is_in_order(this, that):
-	return this["age"] < that["age"]	
+	return this["age"] <= that["age"]	
 
 
 def swapper(this, that):
@@ -25,6 +22,7 @@ def swapper(this, that):
 
 def bubble_sort(dogs):
 	for i in range(len(dogs)):
+		printer(dogs)
 		for j in range(len(dogs)-1-i):
 			if not is_in_order(dogs[j], dogs[j+1]):
 				swapper(dogs[j], dogs[j+1])
@@ -32,4 +30,4 @@ def bubble_sort(dogs):
 
 
 bubble_sort(dogs)
-printer(dogs)		
+		
